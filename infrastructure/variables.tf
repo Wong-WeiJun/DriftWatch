c9387e15cbd65e1a8fd030b8825f9f28ac3532aa
Wong Wei Jun <wong.weijun923@gmail.com>
2026-05-17 16:14:52 +0800

    added alert variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,9 @@ variable "environment" {
   description = "shows deployment environment"
   type        = string
 }
+
+variable "alert_email" {
+  description = "Email address for notifications"
+  type        = string
+}
+
