67dbae57543a0f9001d1f60c3bd42912fb20bc50
Wong Wei Jun <wong.weijun923@gmail.com>
2026-05-17 13:19:03 +0800

    Variables for vpc config added

---
+++ infrastructure/modules/vpc/variables.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  description = "Base name for the bucket"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of AZs available for subnets to deploy into"
+  type        = string
+}
+
+variable "tags" {
+  type = map(string)
+}
