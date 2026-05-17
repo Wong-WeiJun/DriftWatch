f193e4f39814af402bd2bb70ca799494b02a9836
Wong Wei Jun <wong.weijun923@gmail.com>
2026-05-17 13:19:17 +0800

    added resources

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,13 +1,27 @@
-variable "name" {
-  description = "Base name for the bucket"
-  type        = string
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = merge(var.tags, { Name = var.name })
 }
 
-variable "account_id" {
-  description = "AWS account ID used for unique naming"
-  type        = string
+resource "aws_subnet" "public" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = var.azs[count.index]
+  tags              = merge(var.tags, { Name = "${var.name}-public-${var.azs[count.index]}" })
 }
 
-variable "tags" {
-  type = map(string)
+resource "aws_subnet" "private" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = var.azs[count.index]
+  tags              = merge(var.tags, { Name = "${var.name}-private-${var.azs[count.index]}" })
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = merge(var.tags, { Name = var.name })
 }
