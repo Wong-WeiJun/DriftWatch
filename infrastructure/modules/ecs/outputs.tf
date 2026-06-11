0018190ddbf0773c12f18208ccc5713e0ed94e9d
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-11 12:04:19 +0800

    Initial Commit

--- infrastructure/modules/ecs/outputs.tf
+++ infrastructure/modules/ecs/outputs.tf
@@ -13,3 +13,11 @@ output "ecs_service_name" {
 output "ecs_security_group_id" {
   value = aws_security_group.ecs_tasks.id
 }
+
+output "execution_role_arn" {
+  value = aws_iam_role.ecs_execution_role.arn
+}
+
+output "task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
