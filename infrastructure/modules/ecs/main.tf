f14d600dc7483f90f42b817b367ff419bc0a0bf7
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-11 09:29:01 +0800

    added iam role

--- infrastructure/modules/ecs/main.tf
+++ infrastructure/modules/ecs/main.tf
@@ -23,6 +23,26 @@ resource "aws_iam_role_policy_attachment" "ecs_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = var.name
   network_mode             = "awsvpc"
