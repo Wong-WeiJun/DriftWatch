6940a0e4b44169a8d2cff3a2e40412a97f10fdd9
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-11 11:40:22 +0800

    added api_url and alb_dns

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -9,3 +9,11 @@ output "sns_topic_arn" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "api_url" {
+  value = "http://${module.ecs.alb_dns_name}"
+}
+
+output "alb_dns_name" {
+  value = module.ecs.alb_dns_name
+}
